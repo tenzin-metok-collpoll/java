fdf52092f5185bccb847ca5ddd15cd7fd2ad691f
tenzinMetok <tenzin.m@collpoll.com>
2023-06-13 15:28:00 +0530

    first commit

---
+++ Sample.java
@@ -0,0 +1,14 @@
+public class Sample {
+     public static void main(String args[])
+    {
+
+        int a,b,sum=0,product=0;
+        a=10;
+        b=20;
+        sum=a+b;
+        product=a*b;
+        System.out.println("====="+args[0]);
+        System.out.println("Sum="+sum);
+        System.out.println("Product="+product);
+    }
+}
---
+++ index.java
@@ -0,0 +1,7 @@
+class Hello
+{
+        public static void main(String str[]) 
+    {
+        System.out.println(24+452);
+    }
+}
